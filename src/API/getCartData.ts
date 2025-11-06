de81d9f3d052cf7113b4a8ca77a33afd032471e9
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-11-06 10:37:07 +0200

    --

--- src/API/getCartData.ts
+++ src/API/getCartData.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { userCart } from "../slices/productData";
 
-export const API = 'http://localhost:5000'
+export const API = 'https://exclusive-nine-eta.vercel.app/api/test'
 export const APICartData = async () => {
   const res = await fetch(API+"/cart", {
     method: "GET",
